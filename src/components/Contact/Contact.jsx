dc003bf8a110ca07300a4b4a1391eb42239ee1c0
pedroarealtorres <pedro.ts.torres@gmail.com>
2022-04-11 13:22:37 +0100

    Captcha on Contact to avoid Spam + CSS Improvement

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,72 +1,46 @@
-import React, { useRef } from "react";
-import "./contact.css";
-import { MdOutlineEmail } from "react-icons/md";
-import { IoLogoWhatsapp } from "react-icons/io";
-import emailjs from "emailjs-com";
+import React, { useRef } from 'react';
+import './contact.css';
+import { MdOutlineEmail } from 'react-icons/md';
+import { IoLogoWhatsapp } from 'react-icons/io';
 
 export const Contact = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        "service_8usq6l6",
-        "service_8usq6l6",
-        form.current,
-        "Fua7S_RJ-pVQ38n3z"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-
-    e.target.reset();
-  };
-
   return (
-    <section id="contact">
-      <h5>Get in Touch</h5>
+    <section id='contact'>
       <h2>Contact Me</h2>
 
-      <div className="container contact__container">
-        <div className="contact__options">
-          <article className="contact__option">
-            <MdOutlineEmail className="contact__option-icon" />
-            <h4>Email</h4>
-            <h5>pedro.ts.torres@gmail.com</h5>
-            <a
-              href="mailto:pedro.ts.torres@gmail.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Write me
-            </a>
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option contact__option-onDesktop'>
+            <MdOutlineEmail className='contact__option-icon' />
+              <h4>Email</h4>
+              <h5>pedro.ts.torres@gmail.com</h5>
+              <a
+                href='mailto:pedro.ts.torres@gmail.com'
+                target='_blank'
+                rel='noreferrer'
+              >
+                Write me
+              </a>
           </article>
-          <article className="contact__option contact__option-onDesktop">
-            <IoLogoWhatsapp className="contact__option-icon" />
-            <h4>WhatsApp</h4>
-            <h5>+351 914 666 034</h5>
-            <a
-              href="https://wa.me/351914666034"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Write me
-            </a>
+          <article className='contact__option'>
+            <IoLogoWhatsapp className='contact__option-icon' />
+              <h4>WhatsApp</h4>
+              <h5>+351 914 666 034</h5>
+              <a
+                href='https://wa.me/351914666034'
+                target='_blank'
+                rel='noreferrer'
+              >
+                Write me
+              </a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="name" placeholder="Name" required />
-          <input type="email" name="email" placeholder="Email" required />
-          <textarea name="message" rows="3" placeholder="Message" required />
-          <button type="submit" className="btn btn-primary">
+        <form action='https://formspree.io/f/xlezwqkd' method='POST'>
+          <input type='text' name='name' placeholder='Name' required />
+          <input type='email' name='email' placeholder='Email' required />
+          <textarea name='message' rows='5' placeholder='Message' required />
+          <button type='submit' className='btn btn-primary'>
             Send Message
           </button>
         </form>
