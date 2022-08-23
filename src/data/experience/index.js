03e8d08aec78c416212dbf24b96085bfe9714e88
Pedro Torres <pedro.ts.torres@gmail.com>
2022-08-23 23:04:02 +0200

    Added IT Trainer Experience

--- src/data/experience/index.js
+++ src/data/experience/index.js
@@ -1,4 +1,5 @@
 export {default as DATAMON_LOGO} from '../../assets/img/datamon-logo.png';
 export {default as ETM_LOGO} from '../../assets/img/etm-logo.webp';
 export {default as SPMS_LOGO} from '../../assets/img/spms-logo.png';
-export {default as ING_LOGO} from '../../assets/img/ING-logo.png';
+export {default as CESAE_LOGO} from '../../assets/img/cesae-logo.png';
+export {default as ING_LOGO} from '../../assets/img/ing-logo.png';
