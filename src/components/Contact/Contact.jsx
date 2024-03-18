5475ad6f8ef1ac261d7ce033b2fbe9dc99389def
Pedro Torres <pedro.areal.torres@ing.com>
2024-03-18 17:58:10 +0100

    Update contact info

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,34 +10,12 @@ export const Contact = () => {
       <h2>Contact Me</h2>
 
       <div className='container contact__container'>
-        <div className='contact__options'>
-          <article className='contact__option contact__option-onDesktop'>
-            <MdOutlineEmail className='contact__option-icon' />
-              <h4>Email</h4>
-              <h5>pedro.ts.torres@gmail.com</h5>
-              <a
-                href='mailto:pedro.ts.torres@gmail.com'
-                target='_blank'
-                rel='noreferrer'
-              >
-                Write me
-              </a>
-          </article>
-          <article className='contact__option'>
-            <IoLogoWhatsapp className='contact__option-icon' />
-              <h4>WhatsApp</h4>
-              <h5>+351 914 666 034</h5>
-              <a
-                href='https://wa.me/351914666034'
-                target='_blank'
-                rel='noreferrer'
-              >
-                Write me
-              </a>
-          </article>
-        </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action='https://formspree.io/f/xlezwqkd' method='POST' target='_blank'>
+        <form
+          action='https://formspree.io/f/xlezwqkd'
+          method='POST'
+          target='_blank'
+        >
           <input type='text' name='name' placeholder='Name' required />
           <input type='email' name='email' placeholder='Email' required />
           <textarea name='message' rows='5' placeholder='Message' required />
